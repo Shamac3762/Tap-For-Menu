a82915d90b95af87c7f654ae6edd8774f6688378
Shamac3762 <shamac3762@hotmail.co.uk>
2026-06-25 18:14:57 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'TapForMenu | Smart NFC Menus',
+  description: 'Your menu, just a tap away. Upgrade your hospitality venue with beautiful NFC tags.',
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
